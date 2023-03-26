4ee87d1d63eeffc5ad6af858172f3f50887f8ff7
Adnan Sattar <m.adnansattar236@gmail.com>
2023-03-26 22:33:26 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+
+    extend: {
+
+      colors: {
+        darkpink: '#FF00FF',
+        brightred: 'hsl(12, 80%, 56%)',
+        brightgreen: 'hsl(22, 60%, 45%)',
+      },
+
+
+    },
+  },
+  plugins: [],
+}
